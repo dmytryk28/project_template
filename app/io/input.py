c8984b5edbe7ec96654fe28013a6494c93faadbf
Dmytro Shvets <d.shvets@ukma.edu.ua>
2024-03-18 02:36:12 +0200

    Add empty functions to input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def input_console():
+    pass
+
+
+def read_file(file_path):
+    pass
+
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def output_to_console(data):
+    pass
+
+
+def output_to_file(file_path, data):
+    pass
+
+
+def output_to_file_pandas(file_path, data):
+    pass
